7f4f2f1c28d02e72eb97823964b1e1d904625775
azhsmsoso <zzhncut123@163.com>
2023-01-11 11:58:25 +0800

    feat: Implement the zconf initialization function

---
+++ src/main/java/com/github/zeros/core/impl/ZConfFactory.java
@@ -0,0 +1,65 @@
+package com.github.zeros.core.impl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.github.zeros.core.IZConfFactory;
+import com.github.zeros.core.ZConfBuilder;
+
+/**
+ * @author zhaozhenhang <zhaozhenhang@kuaishou.com>
+ * Created on 2023-01-11
+ */
+public class ZConfFactory implements IZConfFactory {
+
+
+    private static final ZConfFactory INSTANCE = new ZConfFactory();
+
+    public ZConfFactory() {}
+
+    public static ZConfFactory getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public ZConfBuilder<Integer> ofInteger(String key, int defaultValue) {
+        return null;
+    }
+
+    @Override
+    public ZConfBuilder<Long> ofLong(String key, long defaultValue) {
+        return null;
+    }
+
+    @Override
+    public ZConfBuilder<Double> ofDouble(String key, double defaultValue) {
+        return null;
+    }
+
+    @Override
+    public ZConfBuilder<Boolean> ofBoolean(String key, boolean defaultValue) {
+        return null;
+    }
+
+    @Override
+    public ZConfBuilder<String> ofString(String key, String defaultValue) {
+        return null;
+    }
+
+    @Override
+    public <T> ZConfBuilder<T> ofJson(String key, T defaultValue, Class<T> clz) {
+        return null;
+    }
+
+    @Override
+    public <V> ZConfBuilder<List<V>> ofJsonList(String key, List<V> defaultValue, Class<V> clz) {
+        return null;
+    }
+
+    @Override
+    public <T extends Map<K, V>, K, V> ZConfBuilder<T> ofJsonMap(String key, T defaultValue, Class<K> keyType,
+            Class<V> valueType) {
+        return null;
+    }
+}
--- src/test/java/com/github/zeros/AppTest.java
+++ src/test/java/com/github/zeros/AppTest.java
@@ -1,6 +1,11 @@
 package com.github.zeros;
 
+import java.util.regex.Pattern;
+
 import org.junit.Test;
+
+import com.github.zeros.core.ZConf;
+import com.github.zeros.core.ZConfs;
 
 /**
  * @author zhaozhenhang <zhaozhenhang@kuaishou.com>
@@ -10,6 +15,8 @@ public class AppTest {
 
     @Test
     public void hello() {
-        System.out.println("azh");
+        ZConf<Pattern> conf = ZConfs.ofString("/tmp/tr", "www.baidu.com")
+                .mapper(Pattern::compile)
+                .build();
     }
 }
